4f68825ac2fb5634bd99ffc09acc44f73f9a9f5f
Alexandre Sanson <alexandre.sanson@telecom-paris.fr>
2022-02-16 18:18:15 +0100

    Implementation itérative non terminée

--- fibo/src/main.rs
+++ fibo/src/main.rs
@@ -7,8 +7,11 @@ fn main() {
 
 fn fibo(n: u32) -> u32 {
 
-    if n == 0 {0} else if n == 1 {1}
-    else {fibo(n-1) + fibo(n-2)}
+    //=========================
+    // IMPLEMENTATION RECURSIVE
+    //=========================
+    //if n == 0 {0} else if n == 1 {1}
+    //else {fibo(n-1) + fibo(n-2)}
 
     // Autre possibilité 
     /*match n {
@@ -16,4 +19,21 @@ fn fibo(n: u32) -> u32 {
         1   => 1,
         _   => fibo(n-1) + fibo(n-2)
     }*/
+
+    //=========================
+    // IMPLEMENTATION ITERATIVE
+    //=========================
+    let mut val: u32 = 0;
+    let mut previous1:u32; 
+    let mut previous2:u32;
+
+    if      n == 0 {return 0;}
+    else if n == 1 {return 1;}
+    else {
+        for _ in 2..=n {
+            previous1 = 1;
+            previous2 = 
+        }
+    }
+
 }
