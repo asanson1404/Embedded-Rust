30252755f5508247178be3aea93bb20686a950a6
Alexandre Sanson <alexandre.sanson@telecom-paris.fr>
2022-02-23 16:32:38 +0100

    Fin du TP1 Prise en main : Fibonacci

--- fibo/src/main.rs
+++ fibo/src/main.rs
@@ -1,11 +1,23 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Number of fibo values to print
+    #[clap(short = 'c', long = "count")]
+    count: u32,
+}
+
 fn main() {
+    let args = Args::parse();
     println!("Hello, world!");
-    for i in 0..=42{
-        println!("fibo[{}] = {}", i, fibo(i));
+    for i  in 0..=args.count {
+        if fibo(i) == None {break;} 
+        else {println!("fibo[{}] = {}", i, fibo(i).unwrap());}
     }
 }
 
-fn fibo(n: u32) -> u32 {
+fn fibo(n: u32) -> Option<u32> {
 
     //=========================
     // IMPLEMENTATION RECURSIVE
@@ -21,21 +33,35 @@ fn fibo(n: u32) -> u32 {
         _   => fibo(n-1) + fibo(n-2)
     }*/
 
+
     //=========================
     // IMPLEMENTATION ITERATIVE
     //=========================
 
-    let mut ret: u32 = 1;
-    let mut previous1:u32 = 0; 
-    let mut previous2:u32;
+    let mut ret: Option<u32> = Some(1);
+    let mut previous1: Option<u32> = Some(0); 
+    let mut previous2: Option<u32>;
 
-    if      n == 0 {return previous1;}
-    else if n == 1 {return ret;}
+    if      n == 0 {Some(0)}
+    else if n == 1 {Some(1)}
     else {
         for _ in 2..=n {
+
             previous2 = previous1;
             previous1 = ret;
-            ret = previous1 + previous2;
+
+            //Arithmétique saturée
+            //ret = previous1.saturating_add(previous2);
+
+            //Arithmétique vérifiée
+            //ret = previous1.checked_add(previous2).unwrap();
+
+            //Retourner None si ret tient pas sur 32 bits
+            ret = previous1.unwrap().checked_add(previous2.unwrap());
+            match ret {
+                None => break,
+                Some(_y) => continue      
+            }
         }
         ret
     }
