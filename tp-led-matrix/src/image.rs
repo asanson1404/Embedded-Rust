60090164bb24f103a0b08d53977bd3288cec5b54
Alexandre Sanson <alexandre.sanson@telecom-paris.fr>
2022-03-28 19:08:24 +0200

    Fin de la partie Color (3.3.2.1) et commencement de la partie Image jusqu'à l'implémentation du trait Default

--- tp-led-matrix/src/image.rs
+++ tp-led-matrix/src/image.rs
@@ -2,12 +2,14 @@
 //! made of pixels
 
 use micromath::F32Ext;
+use core::ops::Mul;
+use core::ops::Div;
 use crate::gamma::gamma_correct;
 
 #[derive(Clone, Copy, Default)]
 /// Data structure which represents an individual
 /// RGB pixel
-struct Color {
+pub struct Color {
     r: u8,
     g: u8,
     b: u8,
@@ -15,9 +17,7 @@ struct Color {
 
 /// Data strucure which represents a whole 
 /// 8x8 image made of pixels
-struct Image {
-
-}
+pub struct Image ([Color; 64]);
 
 impl Color {
 
@@ -31,4 +31,61 @@ impl Color {
         Color { r: gamma_correct(self.r), g: gamma_correct(self.g), b: gamma_correct(self.b) }
     }
 
+}
+
+impl Mul<f32> for Color {
+
+    type Output = Self;
+
+    /// Function which returns another Color whose individual components are multiplied
+    /// by the given floating point value (rhs). Each component will stay in range of an u8.
+    fn mul(self, rhs: f32) -> Self {
+        Color { r: u8_range((self.r as f32 * rhs).round()),
+                g: u8_range((self.g as f32 * rhs).round()),
+                b: u8_range((self.b as f32 * rhs).round()) }
+    }
+
+}
+
+impl Div<f32> for Color {
+
+    type Output = Self;
+
+    /// Function which returns another Color whose individual components are divided
+    /// by the given floating point value (rhs). Each component will stay in range of an u8
+    fn div(self, rhs: f32) -> Self {
+        Color::mul(self, 1.0/rhs)
+    }
+}
+
+impl Image {
+
+    /// Function which returns an image filled with 
+    /// the color given as an argument
+    pub fn new_solid(color: Color) -> Self {
+        Image([color; 64])
+    }
+
+}
+
+pub trait Default {
+    fn default() -> Self;
+}
+
+/// This trait will have a default function to return an image
+/// filled with the default color (BLACK)
+impl Default for Image {
+
+    fn default() -> Self {
+        Image::new_solid(Color{r:0, g:0, b:0})
+    }
+
+}
+
+/// Fonction to be sure a Color component will stay within
+/// the range of an u8 after a multiplication or a division
+fn u8_range (f: f32) -> u8 {
+    if f > 255.0 {u8::MAX}
+    else if f < 0.0 {u8::MIN}
+    else {f as u8}
 }
