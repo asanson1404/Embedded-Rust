d2056178fce947737422deac3d1014292006131f
Alexandre Sanson <alexandre.sanson@telecom-paris.fr>
2022-04-02 18:19:46 +0200

    Fin de la partie Optimizing the setup (3.3.3.4)

--- tp-led-matrix/src/main.rs
+++ tp-led-matrix/src/main.rs
@@ -3,13 +3,22 @@
 
 use cortex_m_rt::entry;
 use stm32l4 as _;   // Just to link it in the executable (it provides the vector table)
+//use panic_rtt_target as _;
+use panic_probe as _;
+//use rtt_target::{rtt_init_print, rprintln};
+use defmt_rtt as _;
 
+/*
 #[panic_handler]
 fn panic_handler(_panic_info: &core::panic::PanicInfo) -> ! {
-    loop {}
+   loop {}
 }
+*/
 
 #[entry]
 fn main() -> ! {
+    //rtt_init_print!();
+    //rprintln!("Hello, world!");
+    defmt::info!("Hello, world!");
     panic!("The program stopped");
 }
