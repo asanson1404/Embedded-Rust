b8a5fcdfe26b3f1079fc791662ba59755e011d58
Alexandre Sanson <alexandre.sanson@telecom-paris.fr>
2022-04-02 18:43:22 +0200

    Fin de la partie Configuring the hardware (3.3.3.5), pret pour la partie GPIO et LED matrix git add .!

--- tp-led-matrix/src/main.rs
+++ tp-led-matrix/src/main.rs
@@ -2,7 +2,8 @@
 #![no_main]
 
 use cortex_m_rt::entry;
-use stm32l4 as _;   // Just to link it in the executable (it provides the vector table)
+//use stm32l4 as _;   // Just to link it in the executable (it provides the vector table)
+use stm32l4xx_hal::{pac, prelude::*};
 //use panic_rtt_target as _;
 use panic_probe as _;
 //use rtt_target::{rtt_init_print, rprintln};
@@ -17,8 +18,25 @@ fn panic_handler(_panic_info: &core::panic::PanicInfo) -> ! {
 
 #[entry]
 fn main() -> ! {
+    let cp = pac::CorePeripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+
+    run(cp, dp)
+}
+
+fn run(_cp: pac::CorePeripherals, dp: pac::Peripherals) -> ! {
+    // Get high-level representations of hardware modules
+    let mut rcc = dp.RCC.constrain();
+    let mut flash = dp.FLASH.constrain();
+    let mut pwr = dp.PWR.constrain(&mut rcc.apb1r1);
+
+    // Setup the clocks at 80MHz using HSI (by default since HSE/MSI are not configured).
+    // The flash wait states will be configured accordingly.
+    let clocks = rcc.cfgr.sysclk(80.MHz()).freeze(&mut flash.acr, &mut pwr);
+
     //rtt_init_print!();
     //rprintln!("Hello, world!");
     defmt::info!("Hello, world!");
     panic!("The program stopped");
 }
+
